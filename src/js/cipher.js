2a4f1d3643dff8a114ec980f9f47817d9cac7d14
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2020-12-13 01:41:35 +0100

    feat: ui improved

--- src/js/cipher.js
+++ src/js/cipher.js
@@ -1,22 +1,8 @@
 /**
- * Display message in UI
- * 
- * @param {String} type     String error|success
- * @param {String} message  Message to display
- */
-function msgBox(type, message) {
-    $('#' + type + '_box').text(message);
-    if ($('#' + type + '_box').hasClass('box-hide')) {
-        $('#' + type + '_box').removeClass('box-hide');
-    }
-}
-
-/**
  * Show answer in UI
  * 
- * @param {String} type     String error|success
  * @param {String} response Answer to display
  */
-function showAnswer(type, response) {
+function showAnswer(response) {
     $('#cipher_text').text(response);
 }
--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,20 +1,56 @@
-$("#encrypt_button").click(function () {
+const keyCheckRegex = /[^A-zòàùèéì]+/gi;
+const errorAlphabeticKey = 'Only alphabetic character allowed for the key';
+const errorPlainTextEmpty = 'Please fill the plain text field';
+const errorKeyEmpty = 'Please fill the key field';
 
+// Encrypt button <CLICK> \\
+$("#encrypt_button").click(function () {
     // Get values
     const key = $("#key").val();
     const plainText = $("#plain_text").val();
-
-    // Handle data
+    // Check if plain text is filled
     if (plainText == '') {
-        msgBox('error', 'Please fill the plain text field');
-    } else if (key == '') {
-        msgBox('error', 'Please fill the key field');
-    } else {
+        $('#plain_text_error').text(errorPlainTextEmpty);
+        if ($('#plain_text_error').hasClass('box-hide')) {
+            $('#plain_text_error').removeClass('box-hide');
+        }
+    }
+    // Check if key is filled
+    if (key == '') {
+        $('#key_error').text(errorKeyEmpty);
+        if ($('#key_error').hasClass('box-hide')) {
+            $('#key_error').removeClass('box-hide');
+        }
+    }
+    // Handle data
+    if (plainText != '' && key != '' && !keyCheckRegex.test(key.currentTarget.value)) {
+        hideMsgBox('error');
         $.post("/cipher/cipher.php", { key: key, text: plainText })
             .done(function (response) {
-                showAnswer('success', response);
+                showAnswer(response);
             }).fail(function (response) {
-                showAnswer('error', response);
+                showAnswer(response);
             });
+    }
+});
+
+// Key input <CHANGE> \\
+$('#key').on('input', function (key) {
+    if (keyCheckRegex.test(key.currentTarget.value)) {
+        $('#key_error').text(errorAlphabeticKey);
+        if ($('#key_error').hasClass('box-hide')) {
+            $('#key_error').removeClass('box-hide');
+        }
+    } else {
+        if (!$('#key_error').hasClass('box-hide')) {
+            $('#key_error').addClass('box-hide');
+        }
+    }
+});
+
+// Plain text <CHANGE> \\
+$('#plain_text').on('input', function (key) {
+    if (!$('#plain_text_error').hasClass('box-hide')) {
+        $('#plain_text_error').addClass('box-hide');
     }
 });
